d4057c858ea242038f23e3ffc543ec53f01c45bc
dantecodex <anshulrajput237@gmail.com>
2024-04-21 23:05:14 +0530

    fetching done

--- script/main.js
+++ script/main.js
@@ -1,9 +1,9 @@
 const apiKey = "1edd9b2dcc777867c2fe3f5da9049109";
 const posterUrl = "https://media.themoviedb.org/t/p/w220_and_h330_face/";
+const accessToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZWRkOWIyZGNjNzc3ODY3YzJmZTNmNWRhOTA0OTEwOSIsInN1YiI6IjU5N2RhMjhlYzNhMzY4NTNmNjAxM2FiYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mdya7liBiNI11VciUGsWMwgeXR_9Bgfsh9RWAnPuMaI";
 
 
 getMovies();
-// newReleaseMovies();
 // comedyMovies();
 // actionMovies();
 // familyMovies();
@@ -11,16 +11,32 @@ getMovies();
 async function getMovies() {
     let location;
     location = document.querySelector(".Popular-Movies");
-    let trending = await fetchMovies("popular");
+    let trending = await fetchMovies("movie/popular");
     printMoviePoster(trending, location);
 
     location = document.querySelector(".Top-Movies");
-    let top = await fetchMovies("top_rated");
+    let top = await fetchMovies("movie/top_rated");
     printMoviePoster(top, location);
 
-    location = document.querySelector(".New-Movies");
-    let newMovie = await fetchMovies("now_playing");
-    printMoviePoster(newMovie, location);
+    location = document.querySelector(".Indian-Movies");
+    let indianMovie = await fetchMovies("movie/now_playing?page=2&region=in");
+    printMoviePoster(indianMovie, location);
+
+    location = document.querySelector(".Comedy-Movies");
+    let comedyMovie = await fetchMovies("discover/movie?page=2&with_genres=35");
+    printMoviePoster(comedyMovie, location);
+
+    location = document.querySelector(".sci-fi-Movies");
+    let scifiMovie = await fetchMovies("discover/movie?with_genres=14%2C878");
+    printMoviePoster(scifiMovie, location);
+
+    location = document.querySelector(".Family-Movies");
+    let familyMovie = await fetchMovies("discover/movie?page=3&with_genres=10751");
+    printMoviePoster(familyMovie, location);
+
+    location = document.querySelector(".War-Movies");
+    let warMovie = await fetchMovies("discover/movie?page=3&with_genres=10752");
+    printMoviePoster(warMovie, location);
 }
 
 
@@ -48,16 +64,32 @@ function printMoviePoster(movies, location) {
 // Fetching Movie Data from API -------------->>>>>>>
 
 
+
 async function fetchMovies(category) {
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${accessToken}`
+        }
+    };
+      
     try {
-        const response = await fetch(`https://api.themoviedb.org/3/movie/${category}?api_key=${apiKey}`);
+        const response = await fetch(`https://api.themoviedb.org/3/${category}`, options);
+        if (!response.ok) {
+            throw new Error('Failed to fetch movies');
+        }
         const data = await response.json();
         return data.results;
-    }
-    catch (error) {
-        console.error(error);
+    } catch (error) {
+        console.error('Error fetching movies:', error);
+        return [];
     }
 }
+
+
+
+
 
 
 // Slider --------------------------------->>>>>>>
